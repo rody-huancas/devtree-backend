642fffc42ab4c7b46118ac4cbe092335f6371cdf
Rody Huancas <rodyhuancas.04@gmail.com>
2024-12-08 17:47:07 -0500

    Feat: slug

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,21 +1,33 @@
 import type { Request, Response } from "express";
+
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
-export const createAccount = async (req: Request, res: Response): Promise<void> => {
+export const createAccount = async ( req: Request, res: Response ) => {
   try {
     const { email, password } = req.body;
+    
+    const slugModule = await import('slug');
+    const slug = slugModule.default;
 
     const userExists = await User.findOne({ email });
-
     if (userExists) {
-      const error = new Error("El Usuario ya está registrado");
+      const error = new Error("El Usuario con ese Email ya está registrado");
       res.status(409).json({ error: error.message });
       return;
     }
 
-    const user = new User(req.body);
+    const handle = slug(req.body.handle, '');
+    const handleExists = await User.findOne({ handle });
+    if (handleExists) {
+      const error = new Error("Nombre de Usuario no disponible");
+      res.status(409).json({ error: error.message });
+      return;
+    }
+
+    const user    = new User(req.body);
     user.password = await hashPassword(password);
+    user.handle   = handle;
 
     await user.save();
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
 export interface IUser {
-    name    : string,
-    email   : string,
-    password: string,
+  handle  : string;
+  name    : string;
+  email   : string;
+  password: string;
 }
 
 const userSchema = new Schema({
+  handle  : { type: String, require: true, trim: true, unique: true },
   name    : { type: String, require: true, trim: true },
   email   : { type: String, require: true, trim: true, unique: true },
   password: { type: String, require: true, trim: true },
